1eb1dd9a47ba4a0bb3d4d8f2d30df0d8b34ac218
samaraCassie <samaracassie@outlook.com>
2024-03-26 02:27:53 -0300

    feat: constructing component BoxInfoPartida

--- src/components/BoxInfoPartida.vue
+++ src/components/BoxInfoPartida.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable no-console -->
 <template>
   <q-page-container>
     <div class="row">
@@ -15,12 +16,15 @@
               <q-card-section>
                 <div class="row">
                   <div class="col-10">
-                    <q-input v-model="number" label="Nome do Jogador" />
+                    <q-input v-model="nomeJogador" :rules="nomeJogadorRules" label="Nome do Jogador" />
                   </div>
                 </div>
                 <div class="row">
+                  <div class="col-2">
+                    <q-btn @click="MaisCartela" label="+ Cartela" q />
+                  </div>
                   <div class="col-10">
-                    <q-input v-model="number" label="Quantidade de Cartelas" q />
+                    <div>{{ QntCartelas }}</div>
                   </div>
                 </div>
               </q-card-section>
@@ -34,19 +38,85 @@
                     <q-btn>Cancelar</q-btn>
                   </div>
                   <div>
-                    <q-btn>Enviar</q-btn>
+                    <q-btn @click="Enviar">Enviar</q-btn>
                   </div>
                 </div>
+              </q-card-section>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <q-card-section>
+                {{ status }}
               </q-card-section>
             </div>
           </div>
         </q-card>
       </div>
     </div>
+    <div class="row" v-for="(Cartela, index) in QntCartelas" :key="index">
+      <div class="col-12">
+        <CartelaAdd />
+      </div>
+    </div>
   </q-page-container>
 </template>
 
 <script setup lang="ts">
-import { number } from 'zod';
+import { db } from '@/db';
+import { ref } from 'vue';
+import CartelaAdd from './CartelaAdd.vue';
+
+let valid:boolean = false;
+const status = ref<string>('');
+const nomeJogador = ref('');
+const nomeJogadorRules = [
+  (value) => {
+    if (!value) return 'Informe o Nome.';
+    valid = true;
+    return valid;
+  },
+  (value) => {
+    if (value?.length > 20) return 'Nome deve ter menos de 20 caracteres.';
+    valid = true;
+    return valid;
+  },
+];
+const QntCartelas = ref(null);
+/* const QntCartelasRules = [
+  (value) => {
+    if (!value) return 'Informe a quantidade de cartelas.';
+    valid = true;
+    return valid;
+  },
+ (value) => {
+    if (value > 5) return 'Limitado à 4 cartelas.';
+    valid = true;
+    return valid;
+  },
+]; */
+
+const MaisCartela = () => {
+  QntCartelas.value += 1;
+};
+
+const Enviar = async () => {
+  if (valid) {
+    try {
+      // eslint-disable-next-line no-plusplus
+      /* for (let i = 0; i <= QntCartelas.value; i++) {
+      } */
+      const id = await db.jogadores.add({
+        Nome: nomeJogador.value,
+        Cartelas: QntCartelas.value,
+      });
+      status.value = `id: ${id}`;
+    } catch (error) {
+      status.value = `Falha ao adicionar a cartela: ${error}`;
+    }
+    console.log(nomeJogador.value);
+    console.log(QntCartelas.value);
+  }
+};
 
 </script>
